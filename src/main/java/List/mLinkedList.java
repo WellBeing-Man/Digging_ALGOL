e385b2fff9b1374946a2b3820999639ba715e760
DG <leesrock113@gmail.com>
2020-03-21 21:40:05 +0900

    gradle

--- src/main/java/List/mLinkedList.java
+++ src/main/java/List/mLinkedList.java
@@ -49,12 +49,21 @@ public class mLinkedList<E> implements List<E> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        if(size==0){
+            return true;
+        }else {
+            return false;
+        }
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        if(indexOf(o)==-1){
+            return false;
+        }else
+        {
+            return true;
+        }
     }
 
     @Override
@@ -75,7 +84,21 @@ public class mLinkedList<E> implements List<E> {
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        ListNode node=head;
+        ListNode delnode;
+        if(indexOf(o)==-1){
+            return false;
+        }else
+        {
+            for(;node.next!=null;node=node.next){
+                if(node.next.data==o){
+                    delnode=node.next;
+                    node.next=node.next.next;
+                    delnode=null;
+                }
+            }
+            return false;
+        }
     }
 
     @Override
@@ -105,26 +128,65 @@ public class mLinkedList<E> implements List<E> {
 
     @Override
     public void clear() {
-
+        ListNode node=head;
+        while (node.next!=null){
+            ListNode temp=node;
+            node=node.next;
+            temp=null;
+        }
     }
 
     @Override
     public E get(int index) {
+        ListNode node=head;
+        int count=0;
+        for(;node.next!=null;node=node.next){
+            if(count==index){
+                return (E)node.data;
+            }
+            count++;
+        }
         return null;
     }
 
     @Override
     public E set(int index, E element) {
+        ListNode node=head;
+        int count=0;
+        for(;node.next!=null;node=node.next){
+            if(count==index){
+                node.data=element;
+            }
+            count++;
+        }
         return null;
     }
 
     @Override
     public void add(int index, E element) {
+        int count=0;
+        ListNode newnode=new ListNode(element);
+        for(ListNode node=head;node.next!=null;node=node.next){
+            if(index==count){
+                newnode.next=node.next;
+                node.next=newnode;
+            }
+            count++;
+        }
 
     }
 
     @Override
     public E remove(int index) {
+        ListNode node=head;
+        int count=0;
+        for(;node.next!=null;node=node.next){
+            if(count+1==index){
+                ListNode temp=node.next;
+                node.next=node.next.next;
+                return (E)temp.data;
+            }
+        }
         return null;
     }
 
@@ -132,7 +194,15 @@ public class mLinkedList<E> implements List<E> {
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        int count=0;
+        ListNode node=head;
+        for(;node.next!=null;node=node.next){
+            if(node.data == o){
+                return count;
+            }
+            count++;
+        }
+        return -1;
     }
 
     @Override
