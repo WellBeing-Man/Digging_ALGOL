ebc5e710f6f3cca9d1635735b4ebf1b58299e30d
DG <leesrock113@gmail.com>
2020-06-29 18:34:28 +0900

    programmers problem

--- src/main/java/BruteForce/main.java
+++ src/main/java/BruteForce/main.java
@@ -6,9 +6,9 @@ public class main {
 
     public static void main(String[] args) {
 
-        NumberGame numberGame=new NumberGame();
+        NCR ncr=new NCR();
         int[] a={5,1,3,7};
         int[] b={2,2,6,8};
-        numberGame.solution(a,b);
+
     }
 }
