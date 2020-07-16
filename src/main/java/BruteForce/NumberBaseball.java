2a8382be3f8ee511b3cb469559a32612f64dc0af
DG <leesrock113@gmail.com>
2020-07-16 19:41:11 +0900

    done disk

--- src/main/java/BruteForce/NumberBaseball.java
+++ src/main/java/BruteForce/NumberBaseball.java
@@ -2,20 +2,48 @@ package BruteForce;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class NumberBaseball {
 
 
-    public void sorting(int[][] balls) {
+    public int solution(int[][] balls) {
+        ArrayList possible=new ArrayList();
 
-        for(int i=1;i<balls.length;i++){
-            int j=i-1;
-            int[] temp=balls[i];
-            while (j>=0  && balls[j][2]>temp[2]){
-                balls[j+1]=balls[j];
-                j--;
+        Arrays.sort(balls, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1]-o2[1]<0){
+                    return o1[2]-o2[2];
+                }else
+
+                return o1[1]-o2[2];
             }
-            balls[j+1]=temp;
-        }
+        });
+
+        getPossible(balls,0,possible);
+
+        return possible.size();
     }
+
+    private void getPossible(int[][] balls,int now, ArrayList possible) {
+
+        if(now>balls.length){
+            possible.add(now);
+            return;
+        }
+
+
+        while (true){
+            String throwing="";
+        //    throwing+=getStrike(balls[now],);
+        }
+
+
+
+
+    }
+
+
 }
--- src/main/java/Heap/DiscController.java
+++ src/main/java/Heap/DiscController.java
@@ -2,88 +2,33 @@ package Heap;
 
 import java.util.*;
 
-//programmers  not done yet!
+//programmers  done!
 public class DiscController{
-    private class Request{
-        int time;
-        int exec;
-
-        public Request(int time, int exec) {
-            this.time = time;
-            this.exec = exec;
-        }
-
-        public int getTime() {
-            return time;
-        }
-
-        public int getExec() {
-            return exec;
-        }
-    }
-
     public int solution(int[][] jobs) {
-        int answer=0;
-        PriorityQueue<Request> requestQueue=new PriorityQueue(new Comparator() {
-            @Override
-            public int compare(Object o1, Object o2) {
-                Request r1=(Request)o1;
-                Request r2=(Request)o2;
-                return r1.getExec()-r2.getExec();
-            }
-        });
-        ArrayList<Request> jobList=new ArrayList<>();
+        int answer = 0;
         int time=0;
+        int next=0;
+        PriorityQueue<int[]> priorityQueue=new PriorityQueue<int[]>(((o1, o2) -> o1[1]-o2[1]));
+        Arrays.sort(jobs,((o1, o2) -> o1[0]-o2[0]));
 
-        for(int[] job:jobs){
-            jobList.add(new Request(job[0],job[1]));
+
+
+        while (next<jobs.length || !priorityQueue.isEmpty()){
+            while (next<jobs.length && jobs[next][0]<=time){
+                priorityQueue.offer(jobs[next++]);
+            }
+
+            if(!priorityQueue.isEmpty()){
+                int[] job=priorityQueue.poll();
+                answer+=((time-job[0]) + job[1]);
+                time+=job[1];
+                job=null;
+                continue;
+            }else {
+                time=jobs[next][0];
+            }
         }
-
-        Collections.sort(jobList, new Comparator() {
-            @Override
-            public int compare(Object o1, Object o2) {
-                Request r1=(Request) o1;
-                Request r2=(Request) o2;
-                int time=r1.getTime()-r2.getTime();
-                int exec=r1.getExec()-r2.getExec();
-                if(time<0)
-                    return exec;
-                 else
-                     return time;
-            }
-        });
-
-        while ( !(jobList.isEmpty() && requestQueue.isEmpty())){
-
-
-            //insert queue
-            if(!jobList.isEmpty()) {
-                while (time > jobList.get(0).getTime()) {
-                    requestQueue.add(jobList.get(0));
-                    jobList.remove(0);
-                    if (jobList.isEmpty()) {
-                        break;
-                    }
-                }
-            }
-
-
-            if(requestQueue.isEmpty()){
-                Request request=jobList.get(0);
-                jobList.remove(request);
-                time+=+request.getExec();
-                answer+=request.getExec();
-            }else{
-                Request request=requestQueue.poll();
-                answer+= (time-request.getTime()+request.getExec());
-                time+=request.getExec();
-            }
-
-        }
-
-
         return answer/jobs.length;
     }
-
-
+    
 }
